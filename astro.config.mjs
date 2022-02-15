304edd0067a16ea29f41b114c87ef258ce6a24dd
Willi Woldt <woldt.willi@gmail.com>
2022-02-15 20:27:19 +0100

    fix: 🐛 vite base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,13 @@ export default {
   buildOptions: {
     // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,         // Generate sitemap (set to "false" to disable)
-    site: 'https://woldtwerk.github.io/astro-windicss/'
   },
   devOptions: {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
   vite: {
+    base: './',
     plugins: [WindiCSS()]
   },
   renderers: [
--- windi.mjs
+++ windi.mjs
@@ -6,6 +6,6 @@ const htmlfiles = glob.sync('./dist/**/index.html')
 
 htmlfiles.map(file => {
   let html = fs.readFileSync(file, 'utf8').toString()
-  html = html.replace(/<script.*src=.*windicss.*><\/script>/, `<link rel="stylesheet" href="${cssfile}">`)
+  html = html.replace(/<script.*src=.*windicss.*><\/script>/, `<link rel="stylesheet" href=".${cssfile}">`)
   fs.writeFileSync(file, html)
 })
